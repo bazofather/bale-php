8f631146c20c3041d9ede8aaea4d2cfb83c306cb
miladimos <milad.jafari6210@gmail.com>
2022-10-08 15:10:47 +0330

    update

--- src/BaleInterface.php
+++ src/BaleInterface.php
@@ -4,5 +4,22 @@ namespace Bazofather\Bale;
 
 interface BaleInterface
 {
-    //
+    public function setWebhook(string $url);
+
+    public function deleteWebhook(string $url);
+
+    public function getMe();
+
+    public function getFile(string $url);
+
+    public function getChat();
+
+    public function getChatAdministrators();
+
+    public function getChatMembersCount();
+
+    public function getChatNumber();
+
+    public function sendInvoice(string $chat_id);
+
 }
